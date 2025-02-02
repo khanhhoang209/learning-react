52c0370ee6a8670caa996a5954f74923f837478a
khanhhoang209 <lehoangkhanh209@gmail.com>
2025-02-02 16:03:32 +0700

    reformat code in tab size = 2

--- src/components/categories/TableCategories.js
+++ src/components/categories/TableCategories.js
@@ -1,47 +1,46 @@
-﻿import axios from "axios";
-import {Table} from "react-bootstrap";
+﻿import {Table} from "react-bootstrap";
 import {useEffect, useState} from "react";
 import {fetchCategories} from "../../services/CategoryService";
 
 const TableCretegories = (props) => {
 
-    const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-    useEffect(() => {
-        //call apis
-        getCategories();
-    }, []);
+  useEffect(() => {
+    //call apis
+    getCategories();
+  }, []);
 
-    const getCategories = async () => {
-        let response = await fetchCategories();
+  const getCategories = async () => {
+    let response = await fetchCategories();
 
-        if (response && response.data && response.data.details) {
-            setCategories(response.data.details.data.categories);
-        }
+    if (response && response.data && response.data.details) {
+      setCategories(response.data.details.data.categories);
     }
+  }
 
-    return (<>
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Status</th>
-                </tr>
-            </thead>
-            <tbody>
-            {categories && categories.length > 0 && categories.map((item, index) => {
-                return (
-                    <tr key={`categories-${index}`}>
-                        <td>{item.id}</td>
-                        <td>{item.name}</td>
-                        <td>{item.status ? "Available" : "Unavailable"}</td>
-                    </tr>
-                )})
-            }
-            </tbody>
-        </Table>
-    </>)
+  return (<>
+    <Table striped bordered hover>
+      <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Status</th>
+      </tr>
+      </thead>
+      <tbody>
+      {categories && categories.length > 0 && categories.map((item, index) => {
+        return (
+          <tr key={`categories-${index}`}>
+            <td>{item.id}</td>
+            <td>{item.name}</td>
+            <td>{item.status ? "Available" : "Unavailable"}</td>
+          </tr>
+        )})
+      }
+      </tbody>
+    </Table>
+  </>)
 }
 
 export default TableCretegories;
